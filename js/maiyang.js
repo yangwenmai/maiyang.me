f54d61a25da9abe8857ee388d7e17593d8879d5c
mai.yang <maiyang@maiyang.local>
2014-10-18 19:22:54 +0800

    all

--- js/maiyang.js
+++ js/maiyang.js
@@ -1,5 +1,5 @@
 /* =============================================================================
-#     FileName: pengx.js
+#     FileName: maiyang.js
 #         Desc: javascript for blog
 #       Author: codepiano
 #        Email: anyexingchen999@qq.com
